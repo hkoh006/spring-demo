bf9ae50f5ee14420363b7a4577113774884e653b
hong <hd.koh290293@gmail.com>
2025-09-19 20:50:36 +0800

    initial

---
+++ src/main/kotlin/org/example/demowithnativeimage/DemoWithNativeImageApplication.kt
@@ -0,0 +1,11 @@
+package org.example.demowithnativeimage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoWithNativeImageApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoWithNativeImageApplication>(*args)
+}
